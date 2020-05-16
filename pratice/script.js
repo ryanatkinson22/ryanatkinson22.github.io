69e492461ba757837e63259a9f6e3d31d67d94b2
ryanatkinson22 <atk17001@byui.edu>
2020-05-16 17:45:50 -0600

    New

--- pratice/script.js
+++ pratice/script.js
@@ -1,4 +1,7 @@
 alert("hi");
+var theDate = (document.lastModified)
+document.getElementById("update").textContent = theDate;
+
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('.ham');
 
