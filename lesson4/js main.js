83a85f56b2404d5c7d9f8eed9376e2c79f5fd642
ryanatkinson22 <atk17001@byui.edu>
2020-05-17 15:41:26 -0600

    new text

--- lesson4/js main.js
+++ lesson4/js main.js
@@ -1,6 +1,4 @@
 
-var theDate = (document.lastModified)
-document.getElementById("update").textContent = theDate;
 
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('.ham');
