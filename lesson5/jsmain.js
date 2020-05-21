d79c6591eff323dbe222686ef8e350f61110a7b8
ryanatkinson22 <atk17001@byui.edu>
2020-05-21 13:59:52 -0600

    New Assignment

---
+++ lesson5/jsmain.js
@@ -0,0 +1,15 @@
+
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener("click",() => {mainnav.classList.toggle('responsive')}, false);
+
+let w_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let m_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let mydate = new Date();
+let weekname = w_names[mydate.getDay()];
+let monthname = m_names[mydate.getMonth()];
+
+const dateoutput = document.querySelector('#date');
+dateoutput.textContent = weekname + ", " + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
